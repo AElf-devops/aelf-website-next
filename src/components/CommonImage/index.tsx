b63d064d5b401418a9bf1e4b3776b59595a3e62f
stacey-zhang <stacey.zhang@aelf.io>
2024-04-11 14:12:04 +0800

    fix: 🐛 fix image

--- src/components/CommonImage/index.tsx
+++ src/components/CommonImage/index.tsx
@@ -1,9 +1,12 @@
+/* eslint-disable @next/next/no-img-element */
 import Image, { ImageProps } from "next/image";
 import { TPartialOption } from "@/types";
 import styles from "./styles.module.scss";
 import clsx from "clsx";
 
-export type TCommonImageProps = TPartialOption<ImageProps, "alt">;
+export type TCommonImageProps = TPartialOption<ImageProps, "alt"> & {
+  src: any;
+};
 
 export default function CommonImage({
   className,
@@ -13,7 +16,8 @@ export default function CommonImage({
 }: TCommonImageProps) {
   return (
     <div className={clsx([styles.commonImageWrap, className])}>
-      <Image quality={100} {...props} alt={alt} priority />
+      {/* <Image quality={100} {...props} alt={alt} priority /> */}
+      <img src={props?.src?.src} alt={alt} />
     </div>
   );
 }
