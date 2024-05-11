7c883b6df63d02130ae66fa325af72181359405f
stacey-zhang <stacey.zhang@aelf.io>
2024-05-11 18:24:02 +0800

    fix: 🐛 fix bug

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -105,7 +105,8 @@ class Request {
 
 const apiServer = new Request({
   // baseURL: process.env.NODE_ENV === "development" ? "/" : urlConfig.cms + "/",
-  baseURL: "/",
+  baseURL: urlConfig.cms + "/",
+  // baseURL: "/",
 });
 
 const defaultRequest = new Request({});
