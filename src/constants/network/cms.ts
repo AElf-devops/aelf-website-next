dc9913c9b0367b0cfc1f02a4c7286aa8389a5c9f
stacey-zhang <stacey.zhang@aelf.io>
2024-05-13 20:01:51 +0800

    fix: 🐛 fix testnet dockerfile

--- src/constants/network/cms.ts
+++ src/constants/network/cms.ts
@@ -2,9 +2,16 @@ const devUrlConfig = {
   cms: "https://test-cms-v2.aelf.com",
 };
 const proUrlConfig = {
-  cms: "https://test-cms-v2.aelf.com"
+  cms: "https://test-cms-v2.aelf.com",
 };
 
 export default function getUrlConfig() {
-  return process.env.NODE_ENV === "development" ? devUrlConfig : proUrlConfig;
+  console.log("NEXT_PUBLIC_APP_ENV", process.env.NEXT_PUBLIC_APP_ENV);
+  if (process.env.NEXT_PUBLIC_APP_ENV === "test") {
+    return devUrlConfig;
+  } else if (process.env.NEXT_PUBLIC_APP_ENV === "production") {
+    return proUrlConfig;
+  } else {
+    return devUrlConfig;
+  }
 }
