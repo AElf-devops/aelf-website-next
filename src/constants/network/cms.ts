64fa7341bd293ae060da295e9f8b96f71f40c7f4
stacey-zhang <stacey.zhang@aelf.io>
2024-05-14 12:02:22 +0800

    feat: 🎸 eidt test cms

--- src/constants/network/cms.ts
+++ src/constants/network/cms.ts
@@ -1,14 +1,17 @@
 const devUrlConfig = {
   cms: "https://test-cms-v2.aelf.com",
 };
-const proUrlConfig = {
+const testUrlConfig = {
   cms: "https://test-cms-v2.aelf.com",
+};
+const proUrlConfig = {
+  cms: "https://cms-v2.aelf.com",
 };
 
 export default function getUrlConfig() {
   console.log("NEXT_PUBLIC_APP_ENV", process.env.NEXT_PUBLIC_APP_ENV);
   if (process.env.NEXT_PUBLIC_APP_ENV === "test") {
-    return devUrlConfig;
+    return testUrlConfig;
   } else if (process.env.NEXT_PUBLIC_APP_ENV === "production") {
     return proUrlConfig;
   } else {
