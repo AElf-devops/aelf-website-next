452ab6c9f22bb7efd0af31489ce7ac7059cd9e26
calla <sizhu.chen@aelf.io>
2024-06-21 11:03:16 +0800

    chore: adjust image

--- src/assets/dapp/index.ts
+++ src/assets/dapp/index.ts
@@ -1,6 +1,6 @@
 export { default as DappAwaken } from "./DappAwaken.png";
 export { default as DappBeanGoTown } from "./DappBeanGoTown.png";
-export { default as DappEbridge } from "./DappEbridge.svg";
+export { default as DappEbridge } from "./DappEbridge.png";
 export { default as DappETransfer } from "./DappETransfer.png";
 export { default as DappForest } from "./DappForest.png";
 export { default as DappPortkey } from "./DappPortkey.png";
