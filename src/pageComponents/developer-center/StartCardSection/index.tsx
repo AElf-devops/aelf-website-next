475f6bd08162444008f3e2ad802f9ea4fdd0480b
calla <sizhu.chen@aelf.io>
2024-06-28 15:37:01 +0800

    chore: adjust link

--- src/pageComponents/developer-center/StartCardSection/index.tsx
+++ src/pageComponents/developer-center/StartCardSection/index.tsx
@@ -14,7 +14,7 @@ import styles from "./styles.module.scss";
 const START_CARD_LIST_CONFIG: IStartCardItemProps[] = [
   {
     text: "Setup your development environment",
-    href: "https://docs.aelf.com/tools/chain-sdk/",
+    href: "https://docs.aelf.com/tools/",
   },
   {
     text: "Create your first web3 dApp",
