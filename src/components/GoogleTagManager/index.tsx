35980359fbd0a5084538491ce55c56f19963259b
calla <sizhu.chen@aelf.io>
2024-07-23 18:32:56 +0800

    fix: remove duplicate GTM script loading by eliminating useEffect in GTM component

--- src/components/GoogleTagManager/index.tsx
+++ src/components/GoogleTagManager/index.tsx
@@ -1,22 +1,6 @@
-import { useEffect } from "react";
 import Script from "next/script";
 
 const GoogleTagManager = ({ gtmId }: { gtmId: string }) => {
-  useEffect(() => {
-    if (!window?.dataLayer) {
-      window.dataLayer = [];
-    }
-    window?.dataLayer.push({
-      "gtm.start": new Date().getTime(),
-      event: "gtm.js",
-    });
-
-    const gtmScript = document.createElement("script");
-    gtmScript.src = `https://www.googletagmanager.com/gtm.js?id=${gtmId}`;
-    gtmScript.async = true;
-    document.head.appendChild(gtmScript);
-  }, [gtmId]);
-
   return (
     <>
       <Script
